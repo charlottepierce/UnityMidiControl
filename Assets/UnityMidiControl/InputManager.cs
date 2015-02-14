88aee3cdd9662f8beef358f3ed7dc7c5280a80a5
Charlotte Pierce <charlotte@malformed-bits.com>
2015-02-14 14:35:20 +1100

    Created InputManager singleton class.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

---
+++ Assets/UnityMidiControl/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace UnityMidiControl {
+	public sealed class InputManager : MonoBehaviour {
+		private static InputManager _instance;
+
+		void Awake() {
+			if (_instance != null) {
+				Debug.LogWarning("Attempting to create more than one input manager. Additional instances will be destroyed.");
+				UnityEngine.Object.Destroy(this);
+				return;
+			}
+
+			_instance = this;
+		}
+
+		void Update() {
+		}
+	}
+}
