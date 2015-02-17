8218eefe4dbaba3c05251f2d06f7cfce5cd3f641
Charlotte Pierce <charlotte@malformed-bits.com>
2015-02-17 17:08:15 +1100

    InputManager behaviour runs automatically - doesn't need to be assigned to a game object explicitly.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- Assets/UnityMidiControl/InputManager.cs
+++ Assets/UnityMidiControl/InputManager.cs
@@ -4,28 +4,39 @@ using System.Collections.Generic;
 
 namespace UnityMidiControl {
 	public sealed class InputManager : MonoBehaviour {
-		private static InputManager _instance;
-
 		private Dictionary<string, int> _keyMappings; // key = key activated, value = trigger; e.g., key = 'x', value = note number 44
 
-		void Awake() {
-			if (_instance != null) {
-				Debug.LogWarning("Attempting to create more than one input manager. Additional instances will be destroyed.");
-				UnityEngine.Object.Destroy(this);
-				return;
-			}
+		private static InputManager _instance;
+		private static InputManager instance {
+			get {
+				if (_instance == null) {
+					InputManager existing = (InputManager)FindObjectOfType(typeof(InputManager));
+					if (existing) {
+						Debug.LogWarning("MIDI input manager is automatically initialised. Don't assign the InputManager behaviour to an object.");
+						_instance = existing;
+					} else {
+						GameObject inputManager = new GameObject("_MidiInputManager"); // underscore to remember the object is hidden
+						_instance = inputManager.AddComponent<InputManager>();
+						DontDestroyOnLoad(inputManager);
+						inputManager.hideFlags = HideFlags.HideInHierarchy;
+					}
+				}
 
-			_instance = this;
+				return _instance;
+			}
+		}
+
+		public void Awake() {
 			_keyMappings = new Dictionary<string, int>();
 		}
 
 		public static void AddKeyMapping(string key, int trigger) {
-			_instance._keyMappings.Add(key, trigger);
+			instance._keyMappings.Add(key, trigger);
 		}
 
 		public static bool GetKey(string name) {
-			if (_instance._keyMappings.ContainsKey(name)) {
-				int trigger = _instance._keyMappings[name];
+			if (instance._keyMappings.ContainsKey(name)) {
+				int trigger = instance._keyMappings[name];
 				return (MidiInput.GetKey(trigger) > 0.0f) || UnityEngine.Input.GetKey(name);
 			} else {
 				return UnityEngine.Input.GetKey(name);
@@ -33,8 +44,8 @@ namespace UnityMidiControl {
 		}
 
 		public static bool GetKeyDown(string name) {
-			if (_instance._keyMappings.ContainsKey(name)) {
-				int trigger = _instance._keyMappings[name];
+			if (instance._keyMappings.ContainsKey(name)) {
+				int trigger = instance._keyMappings[name];
 				return MidiInput.GetKeyDown(trigger) || UnityEngine.Input.GetKeyDown(name);
 			} else {
 				return UnityEngine.Input.GetKeyDown(name);
@@ -42,8 +53,8 @@ namespace UnityMidiControl {
 		}
 
 		public static bool GetKeyUp(string name) {
-			if (_instance._keyMappings.ContainsKey(name)) {
-				int trigger = _instance._keyMappings[name];
+			if (instance._keyMappings.ContainsKey(name)) {
+				int trigger = instance._keyMappings[name];
 				return MidiInput.GetKeyUp(trigger) || UnityEngine.Input.GetKeyUp(name);
 			} else {
 				return UnityEngine.Input.GetKeyUp(name);
