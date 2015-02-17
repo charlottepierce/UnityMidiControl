a3c7875b1703635a3179eacd45d5600044507975
Charlotte Pierce <charlotte@malformed-bits.com>
2015-02-17 17:24:27 +1100

    Added KeyMapEditor window.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

---
+++ Assets/UnityMidiControl/Editor/KeyMapEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace UnityMidiControl.Editor {
+	public class KeyMapEditor : EditorWindow {
+		[MenuItem ("MIDI Input/Edit Key Mappings")]
+		public static void ShowWindow() {
+			EditorWindow.GetWindow(typeof(KeyMapEditor)); // show editor window; create it if it doesn't exist
+		}
+
+		public void OnGUI() {}
+	}
+}
