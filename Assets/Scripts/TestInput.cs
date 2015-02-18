efc602de9693c71eaad4fd869f1dcb3ed78788b7
Charlotte Pierce <charlotte@malformed-bits.com>
2015-02-18 16:24:31 +1100

    Simplified TestInput.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- Assets/Scripts/TestInput.cs
+++ Assets/Scripts/TestInput.cs
@@ -3,11 +3,6 @@ using System.Collections;
 using UnityMidiControl.Input;
 
 public class TestInput : MonoBehaviour {
-	void Start() {
-//		InputManager.AddKeyMapping("x", 60);
-//		Debug.Log("Added mapping for 'x' to be triggered by note number 60 (middle C)");
-	}
-
 	void Update () {
 		if (InputManager.GetKeyDown("x")) {
 			Debug.Log("'x' key pressed");
@@ -15,10 +10,6 @@ public class TestInput : MonoBehaviour {
 
 		if (InputManager.GetKeyUp("x")) {
 			Debug.Log("'x' key released");
-		}
-
-		if (InputManager.GetKey("x")) {
-			Debug.Log("'x' key held");
 		}
 	}
 }
